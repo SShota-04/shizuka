1105950f171b25cba76cf246d855eea090301f96
SShota-04 <nshota69@gmail.com>
2022-09-23 12:34:10 +0900

    forth commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SHIZUKA') }}</title>
+    <title>SHIZUKA</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -99,7 +99,7 @@
         <nav class="header-section">
             <div class="container">
                 <a class="app-title" href="{{ url('/') }}">
-                    {{ config('app.name', 'SHIZUKA') }}
+                    SHIZUKA
                 </a>
                 <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
